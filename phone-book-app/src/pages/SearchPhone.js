2392d24e9c9265d1118101d02ba114b7da504d95
Lea Thumbiran <111317155+leathumbiran02@users.noreply.github.com>
2023-08-20 01:27:20 +0200

    Added code to search for contact details.
    The code checks if the drop down menu and the filter text box has values, if it does it then checks them against the database. If a result was found, it is displayed in an alert box. If a result was not found, an error message is displayed in an alert box. If the drop down or the filter text box or both are empty, an error message is displayed in an alert.

--- phone-book-app/src/pages/SearchPhone.js
+++ phone-book-app/src/pages/SearchPhone.js
@@ -12,10 +12,41 @@ export default function SearchPhone(){
 
     */
 
+    /* State to track the selected phone book name and the filter text: */
     const [selectedPhoneBook, setSelectedPhoneBook] = useState('');
+    const [filterText, setFilterText] = useState('');
 
+    /* Extract the phone book names from the data in the db.json file: */
     const phoneBookNames = phoneBookData.phone_book.map(entry => entry.phone_book_name);
 
+    /* Function to handle the searching of contacts: */
+    const handleSearch = () => {
+        if (selectedPhoneBook && filterText.trim() !== '') {
+            /* Find the selected phone book entry: */
+            const selectedPhoneBookEntry = phoneBookData.phone_book.find(entry => entry.phone_book_name === selectedPhoneBook);
+            const selectedPhoneBookId = selectedPhoneBookEntry ? selectedPhoneBookEntry.id : null;
+        
+            /* Filter the search results based on the phone book name and the filter text that was entered: */
+            const searchResults = phoneBookData.phone_book_details.filter(detail => {
+                const isMatchingPhoneBook = selectedPhoneBookId === null || detail.phone_book_id === selectedPhoneBookId;
+                const isMatchingFilter = 
+                    detail.full_name.toLowerCase().includes(filterText.toLowerCase()) ||
+                    detail.phone_number.toString().includes(filterText);
+        
+                return isMatchingPhoneBook && isMatchingFilter;
+            });
+        
+            /* If the length of the search results is greater than 0 (if a result was found) display it in an alert box: */
+            if (searchResults.length > 0) {
+                alert(`Contact Details:\nFull Name: ${searchResults.map(result => `${result.full_name} \nPhone Number: ${result.phone_number}`).join('\n')}`);
+            } else { /* If the result was not found, display an error message in an alert box: */
+                alert('No Contact Was Found! Please Try Again.');
+            }
+        } else { /* If phone book name or filter text is empty, or both display an error message in an alert box: */
+            alert('Phone Book Name And Filter Text Cannot Be Empty!');
+        }
+    };
+    
     return(
         <div>
             <div class="spacing" style={{ marginTop:'100px' }}></div> {/* Added a div to push the content down on the page:  */}
@@ -37,13 +68,15 @@ export default function SearchPhone(){
 
             <div class="center-headings">
                 <div class="field-group">
-                    <input type="text" name="filter" id="filter" class="input-field" placeholder="Filter" autocomplete="off"></input> {/* Input field for entering a value to filter by: */}
-                    <label for="filer" class="input-label">Filter</label>
+                    <input type="text" name="filter" id="filter" className="input-field" placeholder="Filter" autocomplete="off" value={filterText} onChange={e => setFilterText(e.target.value)}></input> {/* Input field for entering a value to filter by: */}
+                    <label for="filter" class="input-label">Filter</label>
                 </div>
             </div>
 
             <div class="button-group">
-                <input type="submit" name="submit" value="search" class="save-button"></input>
+                {/* Search button: */}
+                <input type="button" name="submit" value="search" class="save-button" onClick={handleSearch}></input>
+                {/* Cancel button: */}
                 <a href="/" class="cancel-button">cancel</a>
             </div>
         </div>
