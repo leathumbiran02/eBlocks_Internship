f96c4d3045ae6cfbea961bf2700011fc9ad4a83c
Lea Thumbiran <111317155+leathumbiran02@users.noreply.github.com>
2023-08-20 00:09:54 +0200

    Added code to save the phone book name to the database, as well as display alert messages to indicate whether it was successful or not.

--- phone-book-app/src/pages/AddPhoneBook.js
+++ phone-book-app/src/pages/AddPhoneBook.js
@@ -1,4 +1,6 @@
 
+import React, {useState} from 'react';
+
 function AddPhoneBook(){
 
     /* Function of this page:
@@ -8,6 +10,54 @@ function AddPhoneBook(){
         -Do relevant styling
 
     */
+
+    /* State to store the value of the phone book name from the text box: */
+    const [phoneBookName, setPhoneBookName] = useState('');
+
+    /* Function to handle when the save button is clicked: */
+    const handleSave = () => {
+
+        if(phoneBookName) {/* Checks if phone book name is empty: */
+            /* Fetches the data from the phone book by passing in the url on the JSON server: */
+            fetch('http://localhost:3000/phone_book')
+            .then((response) => response.json())
+            .then((data) => {
+                /* Fetch the ids of phone books in the table: */
+                const ids = data.map((item) => item.id);
+
+                /* Calculate the next available id to use: */
+                const nextID = Math.max(...ids) + 1;
+
+                /* Create a new phone book object: */
+                const newPhoneBook = {
+                    id: nextID, /* Use the next available id: */
+                    phone_book_name: phoneBookName,
+                };
+
+                /* POST the new phone book data to the JSON server: */
+                fetch('http://localhost:3000/phone_book', {
+                    method:'POST',
+                    headers:{
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(newPhoneBook),
+                })
+                .then((response) => response.json())
+                .then((data) =>{
+                    alert(`${phoneBookName} Phone Book Was Added!`);
+                    /* Clear the data in the text box: */
+                    setPhoneBookName('');
+                })
+                .catch((error) =>{
+                    alert('Phone Book Could Not Be Added! Please Try Again!');
+                });
+            })
+            .catch((error) =>{
+                alert('Could Not Fetch The Phone Books! Please Try Again!');
+            })
+        }
+    };
+
     return(
         <div>
             <div class="spacing" style={{ marginTop:'100px' }}></div> {/* Added a div to push the content down on the page:  */}
@@ -18,11 +68,14 @@ function AddPhoneBook(){
 
             <div class="center-headings">
                 <div class="field-group">
-                    <input type="text" name="phone_book_name" id="phone_book_name" class="input-field" placeholder="Phone Book Name" autocomplete="off"></input> {/* Input field for phone book name: */}
-                    <label for="phone_book_name" class="input-label">Phone Book Name</label>
+                    <input type="text" name="phone_book_name" id="phone_book_name" class="input-field" placeholder="Phone Book Name" autocomplete="off" value={phoneBookName} onChange={(e) => setPhoneBookName(e.target.value)} /> {/* Input field for phone book name: */}
+                    
+                    <label htmlFor="phone_book_name" class="input-label">Phone Book Name</label>
 
                     <div class="button-group">
-                        <input type="submit" name="submit" value="save" class="save-button"></input>
+                        {/* Save button: */}
+                        <button onClick={handleSave} className="save-button">Save</button>
+                        {/* Cancel button: */}
                         <a href="/" class="cancel-button">cancel</a>
                     </div>
                 </div>
